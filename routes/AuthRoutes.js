79063d9a4c6264cd4fde1058dd2ad52f9df197e8
Naman-9 <namancp7@gmail.com>
2023-07-25 16:39:07 +0530

    Ecommerce App

---
+++ routes/AuthRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { createUser, loginUser, checkAuth,  resetPasswordRequest, logout, resetPassword} = require('../controller/AuthController');
+const passport = require('passport');
+
+const router = express.Router();
+
+router.post('/signup', createUser)
+      .post('/login', passport.authenticate('local'), loginUser)
+      .get('/logout', logout)
+      .get('/check', passport.authenticate('jwt'), checkAuth)
+      .post('/reset-password-request', resetPasswordRequest)
+      .post('/reset-password', resetPassword)
+      ;
+exports.router = router;             
